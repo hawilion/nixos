631646b80671421a28657144a3c6e779312ec4e1
Mike Lillie <mlillie57@gmail.com>
2025-12-21 13:25:39 -1000

    Clean flake with pinned FreeCAD

--- flake.nix
+++ flake.nix
@@ -1,28 +1,30 @@
 {
-  description = "Lenovo client NixOS configuration with SOPS and Borg backup";
-  
+  description = "Lenovo client NixOS configuration with pinned FreeCAD and SOPS";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-  
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/c6245e83d836d0433170a16eb185cefe0572f8b8";
-
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/c6245e83d836d0433170a16eb185cefe0572f8b8"; # pinned commit
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
- };
+  };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, sops-nix, ... }:
   let
     system = "x86_64-linux";
+
+    # Overlay to pin FreeCAD from unstable
+    unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
+    overlays = [
+      (final: prev: {
+        freecad = unstable.freecad;
+      })
+    ];
+    pkgs = import nixpkgs { inherit system overlays; };
   in {
     nixosConfigurations.lenovo = nixpkgs.lib.nixosSystem {
       inherit system;
 
-      specialArgs = {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
+      specialArgs = { unstable = unstable; };
 
       modules = [
         ./configuration.nix
--- modules/cad.nix
+++ modules/cad.nix
@@ -1,21 +1,18 @@
 { pkgs, unstable, ... }:
 
 {
-  #### CAD / 3D tools ####
   environment.systemPackages = with pkgs; [
-    unstable.freecad   # pulled from nixos-unstable
+    freecad      # comes from overlay (pinned unstable)
     openscad
     blender
   ];
 
-  #### OpenCASCADE / FreeCAD performance tweaks ####
   environment.sessionVariables = {
     OCC_DISPLAY_DRIVER = "OpenGL";
     OCC_Antialiasing   = "1";
     OCC_Shading        = "Phong";
   };
 
-  #### Fonts (FreeCAD UI stability) ####
   fonts.fontconfig.enable = true;
   fonts.packages = with pkgs; [
     dejavu_fonts
