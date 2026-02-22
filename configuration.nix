65b30f1e4fe1298bdcf78947d7ac51d0b057bfb4
mike <mike@lenovo.local>
2026-02-21 16:57:38 -1000

    updated to make sound work on youtube

--- configuration.nix
+++ configuration.nix
@@ -5,9 +5,17 @@ let
 in
 {
   # ------------------------------------------------
-  # GLOBAL NIXPKGS SETTINGS
+  # GLOBAL NIXPKGS & SYSTEM SETTINGS
   # ------------------------------------------------
   nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.download-buffer-size = 67108864;
+  
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
   # ------------------------------------------------
   # MODULE IMPORTS
@@ -21,12 +29,22 @@ in
   ];
 
   # ------------------------------------------------
-  # BORG BACKUP
+  # BOOTLOADER & KERNEL (Audio Fixes)
   # ------------------------------------------------
-  borgBackup = {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [  
+    "snd_intel_dspcfg.dsp_driver=3"    # Forces SOF driver for Raptor Lake
+    "snd_hda_intel.model=dual-codecs" # Lenovo speaker routing fix
+  ];
+
+  # ------------------------------------------------
+  # HARDWARE / FIRMWARE / SCANNER
+  # ------------------------------------------------
+  hardware.enableAllFirmware = true;
+  hardware.sane = {
     enable = true;
-    clients = backupClients;
-    passphraseCommand = "/root/bin/get-borg-pass.sh";
+    extraBackends = [ pkgs.brscan4 ];
   };
 
   # ------------------------------------------------
@@ -35,146 +53,49 @@ in
   networking = {
     networkmanager.enable = true;
     hostName = "lenovo";
-
     interfaces.enp0s25.ipv4.addresses = [
       { address = "192.168.79.80"; prefixLength = 24; }
     ];
-
     defaultGateway = "192.168.79.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
     firewall.allowedTCPPorts = [ 22 443 8384 22000 ];
     firewall.allowedUDPPorts = [ 22000 21027 ];
   };
 
   # ------------------------------------------------
-  # LOCALE
+  # SERVICES (Audio, Desktop, and Others)
   # ------------------------------------------------
-  time.timeZone = "Pacific/Honolulu";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # ------------------------------------------------
-  # USERS
-  # ------------------------------------------------
-  users.users.mike = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "lp" "scanner" ];
-    packages = with pkgs; [
-      kdePackages.kate
-      kdePackages.kde-cli-tools
-    ];
-  };
-
-  users.users.root.shell = pkgs.bashInteractive;
-
-  # ------------------------------------------------
-  # SYSTEM PACKAGES
-  # ------------------------------------------------
-  environment.systemPackages = with pkgs; [
-    borgbackup sops jq openssh tree ripgrep bash coreutils findutils
-    curl wget parted util-linux age go syncthing xsane nmap
-    plocate neovim logseq nextcloud-client libnotify yq imagemagick
-    img2pdf zenity sane-backends sane-airscan sane-frontends brscan4
-    vim brave kdePackages.kconfig qt6.qttools git pavucontrol sof-firmware 
-  ];
-  
-  # ------------------------------------------------
-  # PROGRAMS
-  # ------------------------------------------------
-  programs = {
-    firefox.enable = true;
-    ssh.startAgent = true;
-    git.enable = true;  
-  };
-
-  # ------------------------------------------------
-  # HARDWARE / SCANNER / FIRMWARE
-  # ------------------------------------------------
-  
- # This ensures the kernel has the "brain" for your Lenovo sound chip
-  hardware.enableAllFirmware = true;
-  hardware.sane = {
-    enable = true;
-    extraBackends = [ pkgs.brscan4 ];
-  };
-
-
-  # ------------------------------------------------
-  # SERVICES
-  # ------------------------------------------------
-services.libinput = {
-    enable = true;
-
-    # Touchpad settings
-    touchpad = {
-      tapping = true;
-      naturalScrolling = true;
-      disableWhileTyping = true;
-      clickMethod = "clickfinger"; # Modern style: 2 fingers for right-click
-      accelSpeed = "0.0";           # Range is -1.0 to 1.0 (0 is default)
-    };
-
-    # Mouse/Trackpoint settings
-    mouse = {
-      accelProfile = "flat";       # Disables mouse acceleration for more precision
-      accelSpeed = "0.0";
-    };
-  }; 
-  # Audio priority for PipeWire
-  security.rtkit.enable = true;
+  security.rtkit.enable = true; # Required for PipeWire real-time priority
 
   services = {
     xserver.enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
+    flatpak.enable = true;
     libreoffice-minimal.enable = true;
-    flatpak.enable = true;   
 
-
-  # PIPEWIRE AUDIO CONFIGURATION
-     # 1. Disable PulseAudio to avoid conflicts
-  services.pulseaudio.enable = false;
-
-  # 2. Real-time priority for audio (prevents crackling)
-  security.rtkit.enable = true;
-
-  # 3. The PipeWire Service
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    
-    # WirePlumber is the "Brain" that finds your speakers
-    wireplumber.enable = true;
-    
-    # This extra config forces WirePlumber to look for hidden Lenovo speakers
-    wireplumber.extraConfig = {
-      "10-disable-camera" = {
-        "monitor.alsa.rules" = [
-          {
-            matches = [ { "node.name" = "~alsa_input.pci.*"; } ];
-            actions = { "update-props" = { "node.disabled" = false; }; };
-          }
-        ];
-      };
-    };
-  };
-    # LOGROTATE (Now correctly nested inside services)
-    logrotate = {
+    # PIPEWIRE AUDIO CONFIGURATION
+    pulseaudio.enable = false; # Disable old PulseAudio
+    pipewire = {
       enable = true;
-      settings = lib.genAttrs (lib.attrNames backupClients) (clientName:
-        let client = backupClients.${clientName}; in
-        {
-          path = "/var/log/borgbackup-${clientName}.log";
-          rotate = 7;
-          daily = true;
-          compress = true;
-          missingok = true;
-          notifempty = true;
-          copytruncate = true;
-        }
-      );
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;      # Allows YouTube/Browsers to play sound
+      wireplumber.enable = true;
+    };
+
+    libinput = {
+      enable = true;
+      touchpad = {
+        tapping = true;
+        naturalScrolling = true;
+        disableWhileTyping = true;
+        clickMethod = "clickfinger";
+      };
+      mouse = {
+        accelProfile = "flat";
+        accelSpeed = "0.0";
+      };
     };
 
     avahi = {
@@ -188,74 +109,84 @@ services.libinput = {
       settings = {
         PermitRootLogin = "prohibit-password";
         PasswordAuthentication = false;
-        PubkeyAuthentication = true;
         AllowUsers = [ "mike" ];
       };
+    };
+
+    logrotate = {
+      enable = true;
+      settings = lib.genAttrs (lib.attrNames backupClients) (clientName: {
+        path = "/var/log/borgbackup-${clientName}.log";
+        rotate = 7;
+        daily = true;
+        compress = true;
+        copytruncate = true;
+      });
     };
   };
 
   # ------------------------------------------------
-  # TMPFILES CLEANUP
+  # BORG BACKUP
+  # ------------------------------------------------
+  borgBackup = {
+    enable = true;
+    clients = backupClients;
+    passphraseCommand = "/root/bin/get-borg-pass.sh";
+  };
+
+  # ------------------------------------------------
+  # USERS
+  # ------------------------------------------------
+  users.users.mike = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "lp" "scanner" "audio" "video" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      kdePackages.kde-cli-tools
+    ];
+  };
+
+  # ------------------------------------------------
+  # SYSTEM PACKAGES
+  # ------------------------------------------------
+  environment.systemPackages = with pkgs; [
+    borgbackup sops jq openssh tree ripgrep bash coreutils 
+    curl wget parted util-linux age syncthing xsane nmap
+    plocate neovim logseq nextcloud-client libnotify yq 
+    imagemagick img2pdf zenity vim brave git pavucontrol 
+    sof-firmware alsa-utils
+  ];
+
+  # ------------------------------------------------
+  # SYSTEMD SERVICES & TMPFILES
   # ------------------------------------------------
   systemd.tmpfiles.rules = [
     "r /etc/systemd/system/nixosServer.timer"
     "r /etc/systemd/system/nixosServer.service"
   ];
 
-  # ------------------------------------------------
-  # BROTHER NETWORK SCANNER SYSTEMD SERVICE
-  # ------------------------------------------------
   systemd.user.services.brother-scan = {
     description = "Register Brother network scanner";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = ''
-        mkdir -p $HOME/.brscan4
-        export BRSANE_CFG_DIR=$HOME/.brscan4
-        ${pkgs.brscan4}/bin/brsaneconfig4 -a name=MFC-L2710DW model=MFC-L2710DW ip=192.168.79.190 || true
-      '';
+      ExecStart = "${pkgs.brscan4}/bin/brsaneconfig4 -a name=MFC-L2710DW model=MFC-L2710DW ip=192.168.79.190";
     };
-    enable = true;
+    wantedBy = [ "default.target" ];
   };
 
   # ------------------------------------------------
-  # BOOTLOADER
+  # MISC CONFIGS
   # ------------------------------------------------
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ 
-  "snd_intel_dspcfg.dsp_driver=3" 
-  "snd_hda_intel.model=dual-codecs" # Common fix for Lenovo dual-speaker setups
-];
-  # ------------------------------------------------
-  # NIX SETTINGS
-  # ------------------------------------------------
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.download-buffer-size = 67108864;
+  time.timeZone = "Pacific/Honolulu";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
-
-  environment.etc."profile.d/custom-path.sh".text = ''
-    export PATH=$HOME/bin:$PATH
-  '';
-
-  # ------------------------------------------------
-  # NEOVIM CONFIG
-  # ------------------------------------------------
+  environment.etc."profile.d/custom-path.sh".text = "export PATH=$HOME/bin:$PATH";
   environment.etc."nvim-config/init.lua".source = ./nvim-config/init.lua;
 
   system.activationScripts.nvim-symlink.text = ''
     mkdir -p /home/mike/.config/nvim
     ln -sf /etc/nixos/nvim-config/init.lua /home/mike/.config/nvim/init.lua
-    chown -R mike:users /home/mike/.config/nvim
   '';
 
-  # ------------------------------------------------
-  # STATE VERSION
-  # ------------------------------------------------
   system.stateVersion = "25.05"; 
 }
