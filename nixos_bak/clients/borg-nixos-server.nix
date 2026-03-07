99bf4fa35add5880644fef36d1e92d47a3d231c9
mike <mike@lenovo.local>
2026-03-06 16:49:42 -1000

    chore: cleanup redundant configuration directory

--- nixos_bak/clients/borg-nixos-server.nix
+++
@@ -1,8 +0,0 @@
-{
-  services.borgBackup.clients.nixos-server = {
-    repo = "/mnt/backupdisk/borg/nixos-server";
-    backupPaths = [ "/etc/nixos" "/var/lib" "/home" ];
-    excludePaths = [ "/var/lib/cache" "/home/*/.cache" ];
- logDir = "/var/log/borg-backup";
-};
-}
